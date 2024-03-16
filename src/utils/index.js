970dd76680432d0e84b9b52218f28207e7cf2bcb
Trần Minh Quang <tmquang0209@gmail.com>
2024-03-16 16:43:20 +0700

    Add bcryptConfig and jsonConfig modules

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,10 @@
-const bcryptConfig = require("./bcrypt");
+const bcryptConfig = require("./bcryptConfig");
 const jwtConfig = require("./jwtConfig");
 const mailConfig = require("./mailConfig");
+const jsonConfig = require("./jsonConfig");
 
 module.exports = {
+    jsonConfig,
     bcryptConfig,
     jwtConfig,
     mailConfig,
--- src/utils/jsonConfig.js
+++ src/utils/jsonConfig.js
@@ -1,11 +1,122 @@
-// format return json
+const formatJson = {
+    success(data) {
+        return {
+            statusCode: 200,
+            success: true,
+            data,
+        };
+    },
 
-const formatJson = (success, message, data) => {
-    return {
-        success,
-        message,
-        data,
-    };
+    error(message) {
+        return {
+            statusCode: 500,
+            success: false,
+            message,
+        };
+    },
+
+    notFound(message) {
+        return {
+            statusCode: 404,
+            success: false,
+            message,
+        };
+    },
+
+    unauthorized(message) {
+        return {
+            statusCode: 401,
+            success: false,
+            message,
+        };
+    },
+
+    forbidden(message) {
+        return {
+            statusCode: 403,
+            success: false,
+            message,
+        };
+    },
+
+    badRequest(message) {
+        return {
+            statusCode: 400,
+            success: false,
+            message,
+        };
+    },
+
+    conflict(message) {
+        return {
+            statusCode: 409,
+            success: false,
+            message,
+        };
+    },
+
+    created(data) {
+        return {
+            statusCode: 201,
+            success: true,
+            data,
+        };
+    },
+
+    noContent() {
+        return {
+            statusCode: 204,
+            success: true,
+        };
+    },
+
+    accepted(data) {
+        return {
+            statusCode: 202,
+            success: true,
+            data,
+        };
+    },
+
+    notImplemented(message) {
+        return {
+            statusCode: 501,
+            success: false,
+            message,
+        };
+    },
+
+    serviceUnavailable(message) {
+        return {
+            statusCode: 503,
+            success: false,
+            message,
+        };
+    },
+
+    tooManyRequests(message) {
+        return {
+            statusCode: 429,
+            success: false,
+            message,
+        };
+    },
+
+    gone(message) {
+        return {
+            statusCode: 410,
+            success: false,
+            message,
+        };
+    },
+
+    methodNotAllowed(message) {
+        return {
+            statusCode: 405,
+            success: false,
+            message,
+        };
+    },
 };
 
 module.exports = formatJson;
