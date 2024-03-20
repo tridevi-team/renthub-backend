93b74f3f6c4a0a110c0bd80fa33ab03fcc81624d
Trần Minh Quang <tmquang0209@gmail.com>
2024-03-20 22:30:57 +0700

    Add FTP configuration and upload functionality

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 "use strict";
-const app = require("express")();
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 // import routes
-const { UserRoute, HouseRoute, ServiceRoute } = require("./src/routes");
+const { UserRoute, HouseRoute, ServiceRoute, RoomRoute } = require("./src/routes");
+const uploadImages = require("./src/services/uploadImages");
 
 // import database config
 require("./src/config/database");
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(fileUpload());
+app.use(express.static("src/public"));
 
 app.use("/users", UserRoute);
 app.use("/houses", HouseRoute);
 app.use("/services", ServiceRoute);
+app.use("/rooms", RoomRoute);
+
+app.post("/upload", uploadImages);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
---
+++ src/services/uploadImages.js
@@ -0,0 +1,66 @@
+const { Client } = require("ssh2");
+const uuid = require("uuid");
+const { formatJson, ApiException, Exception } = require("../utils");
+require("dotenv").config();
+
+const uploadImages = async (req, res) => {
+    // Get the file that was set to our field named "image"
+    const { image } = req.files;
+
+    // If no image submitted, exit
+    if (!image) return res.sendStatus(400);
+
+    try {
+        // get format file
+        const format = image.name.split(".").pop();
+
+        const rename = uuid.v4().concat(".").concat(format);
+        // Move the uploaded image to our upload folder
+        await image.mv(`src/services/upload/${rename}`);
+
+        const client = new Client();
+
+        client.on("ready", () => {
+            console.log("Client connected");
+            client.sftp((err, sftp) => {
+                if (err) {
+                    console.error("SFTP error:", err);
+                    res.status(500).send(err); // Send error response
+                    client.end();
+                    return;
+                }
+
+                sftp.fastPut(`src/services/upload/${rename}`, `images.tmquang.com/uploads/${rename}`, (err) => {
+                    if (err) {
+                        console.error("Upload error:", err);
+                        res.status(500).send(err); // Send error response
+                        client.end();
+                        throw new ApiException(1002, "Error occurred", err);
+                    }
+                    client.end();
+
+                    return res.json(formatJson.success(1001, "Upload success", { url: `https://images.tmquang.com/uploads/${rename}` }));
+                });
+            });
+        });
+
+        client.on("error", (err) => {
+            client.end();
+            throw new ApiException(1002, "Error occurred", err);
+        });
+
+        client.connect({
+            host: process.env.FTP_HOST,
+            port: process.env.FTP_PORT || 22,
+            username: process.env.FTP_USER,
+            password: process.env.FTP_PASSWORD,
+            algorithms: {
+                serverHostKey: ["ssh-rsa", "ssh-dss"],
+            },
+        });
+    } catch (err) {
+        Exception.handle(err, req, res);
+    }
+};
+
+module.exports = uploadImages;
