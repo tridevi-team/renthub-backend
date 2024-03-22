e1b211592757c14363ba056f7b834f3a9eda0c48
Trần Minh Quang <tmquang0209@gmail.com>
2024-03-22 20:31:49 +0700

    Add password hashing validation to user registration and login

--- src/middlewares/validator/Users.js
+++ src/middlewares/validator/Users.js
@@ -7,7 +7,13 @@ const registerValidator = [
     check("fullName", "Please provide your full name.").isLength({ min: 1, max: 50 }),
     check("password", "Password must be at least 8 characters long and contain at least one letter and one number.")
         .isLength({ min: 8 })
-        .matches(/^(?=.*\d)(?=.*[a-z]).{8,}$/),
+        .custom((value) => {
+            if (!value.startsWith("$2") && !value.startsWith("$2a") && !value.startsWith("$2b") && !value.startsWith("$2y")) {
+                throw new Error("Password must be hashed.");
+            }
+
+            return true;
+        }),
     check("confirmPassword", "Please confirm your password.").custom((value, { req }) => {
         if (value !== req.body.password) {
             throw new Error("Passwords do not match.");
@@ -21,7 +27,13 @@ const loginValidator = [
     check("username", "Please provide a valid email address or phone number.").not(),
     check("password", "Password must be at least 8 characters long and contain at least one letter and one number.")
         .isLength({ min: 8 })
-        .matches(/^(?=.*\d)(?=.*[a-z]).{8,}$/),
+        .custom((value) => {
+            if (!value.startsWith("$2") && !value.startsWith("$2a") && !value.startsWith("$2b") && !value.startsWith("$2y")) {
+                throw new Error("Password must be hashed.");
+            }
+
+            return true;
+        }),
 ];
 
 const verifyAccountValidator = [check("email", "Please provide a valid email address.").isEmail(), check("verifyCode", "Please provide the verification code.").isNumeric()];
