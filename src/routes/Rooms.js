1fcb22b6a48c216a80f507a6b3098675a0e38cf3
Trần Minh Quang <tmquang0209@gmail.com>
2024-06-03 16:21:53 +0700

    chore: Update room routes with error handling and middleware

--- src/routes/Rooms.js
+++ src/routes/Rooms.js
@@ -3,10 +3,10 @@ const { roomController } = require("../controllers");
 const { createRooms, deleteRoom, updateRoom, getRoomList, roomDetails } = require("../middlewares/validator");
 const handleErrors = require("../middlewares/handleErrors");
 
-roomRouter.get("/list/:houseId", getRoomList, roomController.getRoomList);
-roomRouter.get("/details/:houseId/:roomId", roomDetails, roomController.roomDetails);
-roomRouter.post("/create/:houseId", createRooms, roomController.create);
-roomRouter.put("/update/:houseId/:roomId", updateRoom, roomController.update);
-roomRouter.delete("/delete/:houseId/:roomId", deleteRoom, roomController.delete);
+roomRouter.get("/list/:houseId", getRoomList, handleErrors, roomController.getRoomList);
+roomRouter.get("/details/:houseId/:roomId", roomDetails, handleErrors, roomController.getRoomDetails);
+roomRouter.post("/create/:houseId", createRooms, handleErrors, roomController.createRoom);
+roomRouter.put("/update/:houseId/:roomId", updateRoom, handleErrors, roomController.updateRoom);
+roomRouter.delete("/delete/:houseId/:roomId", deleteRoom, handleErrors, roomController.deleteRoom);
 
 module.exports = roomRouter;
