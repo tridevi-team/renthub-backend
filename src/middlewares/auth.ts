57a6e4bda41e75f6049fa6acd6feaa3ebde2c29f
Trần Minh Quang <tmquang0209@gmail.com>
2024-07-29 00:15:42 +0700

    refactor: Update auth middleware to use Request, Response, and NextFunction types

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,6 +1,7 @@
 import { ApiException, Exception, jwtToken } from "../utils";
+import { Request, Response, NextFunction } from "express";
 
-const ignoreAuth = async (req: any, res: any, next: Function) => {
+const ignoreAuth = async (req: Request, res: Response, next: NextFunction) => {
     const urls = ["/users/login", "/users/signup", "/users/resendCode", "/users/forgotPassword", "/users/resetPassword", "/users/verifyAccount"];
 
     if (urls.includes(req.originalUrl)) {
