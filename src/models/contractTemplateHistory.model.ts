d98184e5df66167f062ca0681b467d1dc1bb4d72
Trần Minh Quang <tmquang0209@gmail.com>
2024-09-14 10:36:52 +0700

    refactor: update add uuid before insert in models

--- src/models/contractTemplateHistory.model.ts
+++ src/models/contractTemplateHistory.model.ts
@@ -1,12 +1,19 @@
-import { Model } from "objection";
+import { Model, QueryContext } from "objection";
+import { v4 as uuidv4 } from "uuid";
 
 class ContractTemplateHistory extends Model {
+    id: string;
+
     static get tableName() {
         return "contract_template_history";
     }
 
     static get idColumn() {
         return "id";
+    }
+
+    $beforeInsert(queryContext: QueryContext): Promise<any> | void {
+        this.id = this.id || uuidv4();
     }
 
     static get jsonSchema() {
--- src/models/roomContractHistory.model.ts
+++ src/models/roomContractHistory.model.ts
@@ -1,30 +1,19 @@
-/**
-Table: room_contract_history
-Columns:
-id char(36) PK
-room_contract_id char(36)
-room_id char(36)
-contract_id char(36)
-deposit_amount int UN
-deposit_status int UN
-deposit_date datetime
-deposit_refund int UN
-deposit_refund_date datetime
-rental_start_date datetime
-rental_end_date datetime
-status varchar(255)
-action varchar(10)
-created_by char(36)
-created_at datetime */
-import { Model } from "objection";
+import { Model, QueryContext } from "objection";
+import { v4 as uuidv4 } from "uuid";
 
 class RoomContractHistory extends Model {
+    id: string;
+
     static get tableName() {
         return "room_contract_history";
     }
 
     static get idColumn() {
         return "id";
+    }
+
+    $beforeInsert(queryContext: QueryContext): Promise<any> | void {
+        this.id = this.id || uuidv4();
     }
 
     static get jsonSchema() {
