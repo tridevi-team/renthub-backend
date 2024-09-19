5021356032c2c94fc10f7b4cbd0aefb2e99f84e6
Trần Minh Quang <tmquang0209@gmail.com>
2024-09-19 20:34:26 +0700

    Refactor issue and issue history migrations and models

--- src/database/migrations/20240823041735_issues.ts
+++ src/database/migrations/20240823041735_issues.ts
@@ -10,8 +10,10 @@ export async function up(knex: Knex): Promise<void> {
         table.specificType("description", "text").nullable();
         table.json("files").nullable(); // include image, video, pdf, word, etc...
         table.uuid("assign_to").references("id").inTable("users").nullable().onDelete("SET NULL").onUpdate("CASCADE");
-        table.uuid("created_by").references("id").inTable("renters").onDelete("CASCADE").onUpdate("CASCADE");
+        table.uuid("created_by").references("id").inTable("renters").onDelete("SET NULL").onUpdate("CASCADE");
         table.datetime("created_at").defaultTo(knex.fn.now());
+        table.uuid("updated_by").references("id").inTable("users").onDelete("SET NULL").onUpdate("CASCADE");
+        table.datetime("updated_at").defaultTo(knex.fn.now());
     });
 }
 
