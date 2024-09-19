16f9a6f1451024665ffcd92529d95e877bc80daf
Trần Minh Quang <tmquang0209@gmail.com>
2024-09-19 17:19:22 +0700

    Refactor contract template migrations and models

--- src/database/migrations/20240901165401_trigger_contract_template_delete.ts
+++ src/database/migrations/20240901165401_trigger_contract_template_delete.ts
@@ -6,7 +6,11 @@ export async function up(knex: Knex): Promise<void> {
         BEFORE DELETE
         ON contract_template
         FOR EACH ROW
+<<<<<<< Updated upstream
         INSERT INTO contract_template_history (contract_template_id, house_id, name, content, is_status, action, created_by, created_at, updated_by, updated_at)
+=======
+        INSERT INTO contract_template_history (contract_template_id, house_id, name, content, is_active, action, created_by, created_at, updated_by, updated_at)
+>>>>>>> Stashed changes
         VALUES (OLD.id, OLD.house_id, OLD.name, OLD.content, OLD.is_active, "DELETE", OLD.created_by, OLD.created_at, OLD.updated_by, OLD.updated_at);`);
 }
 
