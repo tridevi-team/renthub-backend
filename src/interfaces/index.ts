2093ca7a75073bc0a1c71b15d5da7d39cbfb9868
Trần Minh Quang <tmquang0209@gmail.com>
2024-09-26 21:01:10 +0700

    Add floor interface and room interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,7 @@
 export * from "./address.interface";
+export * from "./floor.interface";
 export * from "./house.interface";
 export * from "./pagination.interface";
 export * from "./renter.interface";
+export * from "./room.interface";
 export * from "./user.interface";
