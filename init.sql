5a4f997caa6b591b2bdb29fde8df4d2248e21879
Trần Minh Quang <tmquang0209@gmail.com>
2024-09-28 00:25:39 +0700

    Refactor: Add Dockerfile and docker-compose.yaml

---
+++ init.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE IF NOT EXISTS renthub_dev;
--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,8 +1,34 @@
+import "dotenv/config";
 import { createClient } from "redis";
+
+const nodeEnv = process.env.NODE_ENV || "development";
+let redisUrl = "";
+let redisDb = 0;
+
+switch (nodeEnv) {
+    case "development":
+        redisUrl = process.env.REDIS_DEV_URL || "redis://localhost:6379/0";
+        redisDb = Number(process.env.REDIS_DEV_DB) || 0;
+        break;
+    case "production":
+        redisUrl = process.env.REDIS_PROD_URL || "redis://localhost:6379/0";
+        redisDb = Number(process.env.REDIS_PROD_DB) || 0;
+        break;
+    case "staging":
+        redisUrl = process.env.REDIS_LOCAL_URL || "redis://localhost:6379/0";
+        redisDb = Number(process.env.REDIS_LOCAL_DB) || 0;
+        break;
+    default:
+        redisUrl = process.env.REDIS_DEV_URL || "redis://localhost:6379/0";
+        redisDb = Number(process.env.REDIS_DEV_DB) || 0;
+        break;
+}
+
+redisUrl += redisDb;
 
 const redisClient = async () =>
     await createClient({
-        url: "redis://localhost:6379/1",
+        url: redisUrl,
     })
         .on("error", (error) => {
             console.error(error);
