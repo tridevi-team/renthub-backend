bdd5bb541e9177c9550fc0581f7b2e9da2e28cc2
Trần Minh Quang <tmquang0209@gmail.com>
2024-11-25 09:57:53 +0700

    feat: export currentTime utility and update tsconfig.json paths for clarity

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,6 +4,7 @@ export { default as apiResponse } from "./apiResponse";
 export { default as bcrypt } from "./bcrypt";
 export { default as camelToSnake } from "./camelToSnake";
 export * from "./crypto";
+export * from "./currentTime";
 export { default as ApiException } from "./Exceptions/ApiException";
 export { default as Exception } from "./Exceptions/Exception";
 export { default as jwtToken } from "./jwt";
