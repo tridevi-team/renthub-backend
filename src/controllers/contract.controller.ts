398e62704383753888ecc2c93acd5b5ebfc0279a
Trần Minh Quang <tmquang0209@gmail.com>
2024-11-28 17:03:41 +0700

    refactor: add cache for contract

--- src/controllers/contract.controller.ts
+++ src/controllers/contract.controller.ts
@@ -1,8 +1,11 @@
 import { ContractStatus, messageResponse } from "@enums";
 import { ContractRequest, RoomContractRequest } from "@interfaces";
 import { ContractService, RenterService } from "@services";
-import { apiResponse, Exception, snakeToCamel } from "@utils";
+import { apiResponse, Exception, RedisUtils, snakeToCamel } from "@utils";
 import { Model } from "objection";
+
+const TEMPLATE_PREFIX: string = "templates";
+const CONTRACT_PREFIX: string = "contracts";
 
 class ContractController {
     static async createContractTemplate(req, res) {
@@ -20,6 +23,9 @@ class ContractController {
                 createdBy: user.id,
                 updatedBy: user.id,
             });
+
+            // delete all cache
+            await RedisUtils.deletePattern(`${TEMPLATE_PREFIX}:*`);
 
             return res.json(apiResponse(messageResponse.CREATE_TEMPLATE_SUCCESS, true, newContract));
         } catch (e) {
@@ -102,6 +108,10 @@ class ContractController {
                 trx
             );
             trx.commit();
+
+            // delete all cache
+            await RedisUtils.deletePattern(`${CONTRACT_PREFIX}:*`);
+
             return res.json(apiResponse(messageResponse.CREATE_CONTRACT_SUCCESS, true, newContract));
         } catch (e) {
             trx.rollback();
@@ -111,8 +121,20 @@ class ContractController {
 
     static async getContractTemplateDetails(req, res) {
         const { templateId } = req.params;
+        const key = `${TEMPLATE_PREFIX}:${templateId}:details`;
         try {
+            const isExistsCache = await RedisUtils.isExists(key);
+            if (isExistsCache) {
+                const contract = await RedisUtils.getSetMembers(key);
+                return res.json(
+                    apiResponse(messageResponse.GET_TEMPLATE_DETAILS_SUCCESS, true, JSON.parse(contract[0]))
+                );
+            }
+
             const contract = await ContractService.findOneContractTemplate(templateId);
+
+            // set cache
+            await RedisUtils.setAddMember(key, JSON.stringify(contract));
 
             return res.json(apiResponse(messageResponse.GET_TEMPLATE_DETAILS_SUCCESS, true, contract));
         } catch (e) {
@@ -123,12 +145,28 @@ class ContractController {
     static async getContractTemplates(req, res) {
         const { houseId } = req.params;
         const { filter = [], sort = [], pagination } = req.query;
+
+        const key = RedisUtils.generateCacheKeyWithFilter(`${TEMPLATE_PREFIX}:${houseId}:list`, {
+            filter,
+            sort,
+            pagination,
+        });
+
         try {
+            const isExistsCache = await RedisUtils.isExists(key);
+            if (isExistsCache) {
+                const contracts = await RedisUtils.getSetMembers(key);
+                return res.json(apiResponse(messageResponse.GET_TEMPLATE_LIST_SUCCESS, true, JSON.parse(contracts[0])));
+            }
+
             const contracts = await ContractService.findAllContractTemplate(houseId, {
                 filter,
                 sort,
                 pagination,
             });
+
+            // set cache
+            await RedisUtils.setAddMember(key, JSON.stringify(contracts));
 
             return res.json(apiResponse(messageResponse.GET_TEMPLATE_LIST_SUCCESS, true, contracts));
         } catch (e) {
@@ -138,8 +176,22 @@ class ContractController {
 
     static async getRoomContractDetails(req, res) {
         const { contractId } = req.params;
+
+        const key = `${CONTRACT_PREFIX}:${contractId}:details`;
+
         try {
+            const isExistsCache = await RedisUtils.isExists(key);
+            if (isExistsCache) {
+                const contract = await RedisUtils.getSetMembers(key);
+                return res.json(
+                    apiResponse(messageResponse.GET_CONTRACT_DETAILS_SUCCESS, true, JSON.parse(contract[0]))
+                );
+            }
+
             const contract = await ContractService.findOneRoomContract(contractId);
+
+            // set cache
+            await RedisUtils.setAddMember(key, JSON.stringify(contract));
 
             return res.json(apiResponse(messageResponse.GET_CONTRACT_DETAILS_SUCCESS, true, snakeToCamel(contract)));
         } catch (e) {
@@ -152,7 +204,19 @@ class ContractController {
         const { filter = [], sort = [], pagination } = req.query;
         const user = req.user;
         const { isApp } = req;
+        const key = RedisUtils.generateCacheKeyWithFilter(`${CONTRACT_PREFIX}:${roomId}:list_isApp_${isApp}`, {
+            filter,
+            sort,
+            pagination,
+        });
+
         try {
+            const isExistsCache = await RedisUtils.isExists(key);
+            if (isExistsCache) {
+                const contracts = await RedisUtils.getSetMembers(key);
+                return res.json(apiResponse(messageResponse.GET_CONTRACT_LIST_SUCCESS, true, JSON.parse(contracts[0])));
+            }
+
             if (isApp && user.role === "renter") {
                 // this is for renter
                 console.log("[RENTER]" + user.name + " is getting contract list");
@@ -161,6 +225,9 @@ class ContractController {
                     sort,
                     pagination,
                 });
+
+                // set cache
+                await RedisUtils.setAddMember(key, JSON.stringify(contracts));
 
                 return res.json(apiResponse(messageResponse.GET_CONTRACT_LIST_SUCCESS, true, snakeToCamel(contracts)));
             }
@@ -171,6 +238,9 @@ class ContractController {
                 pagination,
             });
 
+            // set cache
+            await RedisUtils.setAddMember(key, JSON.stringify(contracts));
+
             return res.json(apiResponse(messageResponse.GET_CONTRACT_LIST_SUCCESS, true, snakeToCamel(contracts)));
         } catch (e) {
             Exception.handle(e, req, res);
@@ -180,13 +250,28 @@ class ContractController {
     static async getRoomContractsByHouse(req, res) {
         const { houseId } = req.params;
         const { filter = [], sort = [], pagination } = req.query;
+
+        const key = RedisUtils.generateCacheKeyWithFilter(`${CONTRACT_PREFIX}:${houseId}:list`, {
+            filter,
+            sort,
+            pagination,
+        });
+
         try {
+            const isExistsCache = await RedisUtils.isExists(key);
+            if (isExistsCache) {
+                const contracts = await RedisUtils.getSetMembers(key);
+                return res.json(apiResponse(messageResponse.GET_CONTRACT_LIST_SUCCESS, true, JSON.parse(contracts[0])));
+            }
+
             const contracts = await ContractService.findAllRoomContractByHouse(houseId, {
                 filter,
                 sort,
                 pagination,
             });
-            console.log("🚀 ~ ContractController ~ getRoomContractsByHouse ~ contracts:", contracts);
+
+            // set cache
+            await RedisUtils.setAddMember(key, JSON.stringify(contracts));
 
             return res.json(apiResponse(messageResponse.GET_CONTRACT_LIST_SUCCESS, true, snakeToCamel(contracts)));
         } catch (e) {
@@ -198,6 +283,7 @@ class ContractController {
         const user = req.user;
         const { templateId } = req.params;
         const { name, content, isActive, landlord }: ContractRequest = req.body;
+
         try {
             const updatedContract = await ContractService.updateContractTemplate(templateId, {
                 name,
@@ -206,6 +292,9 @@ class ContractController {
                 isActive,
                 updatedBy: user.id,
             });
+
+            // delete all cache
+            await RedisUtils.deletePattern(`${TEMPLATE_PREFIX}:*`);
 
             return res.json(apiResponse(messageResponse.UPDATE_TEMPLATE_SUCCESS, true, snakeToCamel(updatedContract)));
         } catch (e) {
@@ -242,6 +331,9 @@ class ContractController {
                 updatedBy: user.id,
             });
 
+            // delete all cache
+            await RedisUtils.deletePattern(`${CONTRACT_PREFIX}:*`);
+
             return res.json(apiResponse(messageResponse.UPDATE_CONTRACT_SUCCESS, true, snakeToCamel(updatedContract)));
         } catch (e) {
             Exception.handle(e, req, res);
@@ -262,6 +354,10 @@ class ContractController {
                 console.log("[LANDLORD]" + user.fullName + " is updating contract status");
                 await ContractService.updateRoomContractStatusByLandlord(contractId, status, user.id);
             }
+
+            // delete all cache
+            await RedisUtils.deletePattern(`${CONTRACT_PREFIX}:*`);
+
             res.json(apiResponse(messageResponse.CONTRACT_STATUS_UPDATED_SUCCESS, true));
         } catch (e) {
             Exception.handle(e, req, res);
@@ -274,6 +370,9 @@ class ContractController {
         try {
             await ContractService.deleteContractTemplate(templateId, user.id);
 
+            // delete all cache
+            await RedisUtils.deletePattern(`${TEMPLATE_PREFIX}:*`);
+
             return res.json(apiResponse(messageResponse.DELETE_TEMPLATE_SUCCESS, true));
         } catch (e) {
             Exception.handle(e, req, res);
@@ -285,6 +384,9 @@ class ContractController {
         const { contractId } = req.params;
         try {
             await ContractService.deleteRoomContract(contractId, user.id);
+
+            // delete all cache
+            await RedisUtils.deletePattern(`${CONTRACT_PREFIX}:*`);
 
             return res.json(apiResponse(messageResponse.DELETE_CONTRACT_SUCCESS, true));
         } catch (e) {
