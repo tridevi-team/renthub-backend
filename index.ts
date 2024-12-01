74e7dc22763d46bc571568d4d6c393f4067015ba
Trần Minh Quang <tmquang0209@gmail.com>
2024-12-01 23:35:12 +0700

    fix: increase rate limit to allow more requests per 15 minutes

--- index.ts
+++ index.ts
@@ -40,7 +40,7 @@ app.use("/api-docs", serve, setup(swaggerSpec));
 // rate limit config
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 10000,
     message: "Too many requests from this IP, please try again after 15 minutes",
 });
 
